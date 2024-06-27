27d54cc00c1683fd56b8532ed55b6e847608d81c
Happiness Chuks Ada <chukshappiness81@gmail.com>
2024-06-27 15:08:10 +0000

    My first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function message(){
+    alert("Welcome To My Website.!!!!");
+}
+
+function something(){
+    let person  = prompt("What is your name: ");
+    alert("Nice to meet you, " +person)
+}
